138f9a4d664a2b91f3f867f7a8c8beb9849778bb
AltBroom8 <gowing.carlos@gmail.com>
2023-03-16 21:04:23 +0100

    Version 1.3

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let carton1=[];
 let carton2=[];
+let cajon1= document.querySelector(".cajon1")
 let vector1=Array.from(document.querySelectorAll(".cajita"));
 let vector2=Array.from(document.querySelectorAll(".cajita2"));
 var num=0;
@@ -42,6 +43,67 @@ for (let i=0;i<15;i++){
     vector2[i].innerText = carton2[i];
 }
 
+
+
+
+let cambia_carton1=document.createElement("button");
+let cambia_carton2=document.createElement("button");
+let body =document.querySelector("body");
+body.appendChild(cambia_carton1);
+body.appendChild(cambia_carton2);
+cambia_carton1.classList.add("cambia","cambia1");
+cambia_carton2.classList.add("cambia","cambia2");
+cambia_carton1.innerText="CAMBIA!!"
+cambia_carton2.innerText="CAMBIA!!"
+cambia_carton1.addEventListener("click",(e)=>{
+    for (let i=0;i<15;i++){
+        vector1.pop();
+        carton1.pop();
+    }
+    for (let i=0;i<15;i++){
+        num=0;
+        let flag=false;
+        do{
+            flag=false;
+            num=(Math.floor(Math.random()*89)+1);
+            for(let j = 0; j < carton1.length; j++){
+                if (num==carton1[j]){
+                    flag=true;
+                }
+            }
+        }while(flag==true);
+        carton1.push(num);
+    }
+    vector1=Array.from(document.querySelectorAll(".cajita"));
+    for (let i=0;i<15;i++){
+        vector1[i].innerText = carton1[i];
+    }
+})
+cambia_carton2.addEventListener("click",(e)=>{
+    for (let i=0;i<15;i++){
+        vector2.pop();
+        carton2.pop();
+    }
+    for (let i=0;i<15;i++){
+        num=0;
+        let flag=false;
+        do{
+            flag=false;
+            num=(Math.floor(Math.random()*89)+1);
+            for(let j = 0; j < carton1.length; j++){
+                if (num==carton2[j]){
+                    flag=true;
+                }
+            }
+        }while(flag==true);
+        carton2.push(num);
+    }
+    vector2=Array.from(document.querySelectorAll(".cajita2"));
+    for (let i=0;i<15;i++){
+        vector2[i].innerText = carton2[i];
+    }
+});
+
 let contador=-1;
 let muchosNumeros = [];
 let muchosNumeros2 = [];
@@ -64,30 +126,23 @@ muchosNumeros.push(num);
 
 
 
-
-
-
-let cambia_carton1=document.createElement("button");
-let cambia_carton2=document.createElement("button");
-let body =document.querySelector("body");
-body.appendChild(cambia_carton1);
-body.appendChild(cambia_carton2);
-cambia_carton1.classList.add("cambia","cambia1");
-cambia_carton2.classList.add("cambia","cambia2");
-cambia_carton1.innerText="CAMBIA!!"
-cambia_carton2.innerText="CAMBIA!!"
-
-
-
-
-
-
-
-
 let miboton= document.querySelector("#miboton");
 miboton.addEventListener("click",(e)=>{
 bandera_boton=true;
+if (bandera_boton) {
+    cambia_carton1.classList.add("cambia_desaparece");
+    cambia_carton2.classList.add("cambia_desaparece");
+    cambia_carton1.addEventListener("transitionend", function() {
+        body.removeChild(cambia_carton1);
+    });
+    cambia_carton1.addEventListener("transitionend", function() {
+        body.removeChild(cambia_carton2);
+    });
+}
+
+
 contador++;
+
 if(contador<89){
     let nuevo = document.createElement("div");
     let recuadro=document.querySelector("#recuadro");
@@ -112,6 +167,27 @@ if(contador<89){
 
 
 });
+let miflag=false;
+for(let i=0;i<5;i++){
+if(vector1[i].classList.contains("tachado")){
+    miflag=true;
+    console.log(`${i} es true`)
+}
+else{
+    miflag=false;
+    break;
+}
+}
+if(miflag){
+console.log(`El número ${vector1[i].innerText} del cartón 1 ha sido tachado.`)
+let tachado1= document.createElement("div");
+cajon1.appendChild(tachado1);
+tachado1.classList.add("linea", "t1");
+tachado1.addEventListener("animationend",()=>{
+    cajon1.removeChild(tachado1);
+})
+}
+
 
 
 
