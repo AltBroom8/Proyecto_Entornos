fc9bc447e8b1853348abfa6059a4837e99187319
AltBroom8 <gowing.carlos@gmail.com>
2023-03-16 19:38:00 +0100

    Version 1.1

--- script.js
+++ script.js
@@ -1,45 +1,79 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let carton1=[];
-    let carton2=[];
-    let vector1=Array.from(document.querySelectorAll(".cajita"));
-    let vector2=Array.from(document.querySelectorAll(".cajita2"));
-    for (let i=0;i<15;i++){
-        var num=0;
-        let flag=false
-        do{
-            num=(Math.floor(Math.random()*80)+10);
-            for(let j=0;j<carton1.lenght;j++){
-                if (num==carton1[j]){
-                    flag=true;
-                }
+let carton1=[];
+let carton2=[];
+let vector1=Array.from(document.querySelectorAll(".cajita"));
+let vector2=Array.from(document.querySelectorAll(".cajita2"));
+var num=0;
+for (let i=0;i<15;i++){
+    num=0;
+    let flag=false;
+    do{
+        flag=false;
+        num=(Math.floor(Math.random()*80)+10);
+        for(let j = 0; j < carton1.length; j++){
+            if (num==carton1[j]){
+                flag=true;
             }
-        }while(flag==true);
-        
-        carton1.push(num);
-    }
-    for (let i=0;i<15;i++){
-        vector1[i].innerText = carton1[i];
-    }
+        }
+    }while(flag==true);
+    
+    carton1.push(num);
+}
+for (let i=0;i<15;i++){
+    vector1[i].innerText = carton1[i];
+}
 
-    //SEPARACION 
-    for (let i=0;i<15;i++){
-        var num=0;
-        let flag=false
-        do{
-            flag=false;
-            num=(Math.floor(Math.random()*80)+10);
-            for(let j=0;j<carton2.lenght;j++){
-                if (num==carton2[j]){
-                    flag=true;
-                }
+for (let i=0;i<15;i++){
+    num=0;
+    let flag=false;
+    do{
+        flag=false;
+        num=(Math.floor(Math.random()*80)+10);
+        for(let j=0;j<carton2.length;j++){
+            if (num==carton2[j]){
+                flag=true;
             }
-        }while(flag==true);
-        
-        carton2.push(num);
+        }
+    }while(flag==true);
+    
+    carton2.push(num);
+}
+for (let i=0;i<15;i++){
+    vector2[i].innerText = carton2[i];
+}
+
+let contador=-1;
+let muchosNumeros = [];
+let muchosNumeros2 = [];
+for (let i=0;i<80;i++){
+let flag=false;
+num=0;
+do{
+    flag=false;
+    num=(Math.floor(Math.random()*80)+10);
+    for(let j=0;j<muchosNumeros.length;j++){
+        if (num==muchosNumeros[j]){
+            flag=true;
+        }
     }
-    for (let i=0;i<15;i++){
-        vector2[i].innerText = carton2[i];
-    }
+}while(flag==true);
+
+muchosNumeros.push(num);
+
+}
+console.log(muchosNumeros);
+let miboton= document.querySelector("#miboton");
+miboton.addEventListener("click",(e)=>{
+contador++;
+if(contador<80){
+    let nuevo = document.createElement("div");
+    let recuadro=document.querySelector("#recuadro");
+    recuadro.appendChild(nuevo);
+    nuevo.innerText=muchosNumeros[contador];
+    nuevo.classList.add("cajita3");
+}
+
 
 });
+
+
 
